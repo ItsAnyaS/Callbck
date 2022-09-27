3bc3fd8cdf3ee77f1afd895152a2d30b6417b810
anastasia <anyasirman@outlook.com>
2022-09-27 16:59:41 -0400

    added onclick handlers for login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,26 @@
 import '../styles/Login.css'
+import { useState, useEffect } from 'react'
 const Login = () => {
+    const [loginType, setLoginType] = useState('dancer')
 return (
     <main>
-        Hello world
+        {loginType == 'dancer' && 
+        <section>
+            <form>
+                <input placeholder='Email' type={'email'}/>
+                <input placeholder='Password' type={'password'}/>
+                <button>Login</button>
+                <button onClick={()=> {setLoginType('company')}}>Login as company</button>
+            </form>
+        </section>}
+        { loginType == 'company' &&
+            <section>
+                <input placeholder='Email' type={'email'}/>
+                <input placeholder='Password' type={'password'}/>
+                <button>Login</button>
+                <button onClick={()=> {setLoginType('dancer')}}>Login as dancer</button>
+            </section>
+        }
     </main>
 )
 }
