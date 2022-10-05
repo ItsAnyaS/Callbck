1eb3e7eb55db03a494226192081d83b88f4fc16b
anastasia <anyasirman@outlook.com>
2022-10-05 11:29:45 -0400

    removed iframe support

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationsController <  ApplicationController
     skip_before_action :verify_authenticity_token
-    after_action :allow_iframe, only: :applicaitons_by_listings
+    # after_action :allow_iframe, only: :applicaitons_by_listings
 
     def show 
         application = Application.find_by(id: params[:id])
@@ -47,14 +47,16 @@ class ApplicationsController <  ApplicationController
     end
 
     def applicaitons_by_listings
-        response.headers.except! 'X-Frame-Options'
+        # response.headers.except! 'X-Frame-Options'
         applications = Application.where(listing_id: params[:id])
+        if applications
         dancers = applications.map { |app| Dancer.find_by(id: app.dancer_id)}
         dancers = dancers.map { |dancer| {first_name: dancer.first_name, last_name: dancer.last_name, gender: dancer.gender, years_of_experience: dancer.years_of_experience, email: dancer.email, headshot: dancer.image_url, resume: dancer.resume_url, dance_reel: dancer.dance_reel}}
-        # puts dancers
-         apps = applications.map.with_index {|app, index| {id: app.id, listing_id: app.listing_id, company_id: app.company_id, status: app.status, dancer: dancers[index] }}
-         puts apps
+        apps = applications.map.with_index {|app, index| {id: app.id, listing_id: app.listing_id, company_id: app.company_id, status: app.status, dancer: dancers[index] }}
         render json: apps
+        else 
+            render json: {message: 'no applications'}, status: 404
+        end
     end
 
     def destroy 
