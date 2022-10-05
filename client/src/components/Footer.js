a5609649ae4497f8a45f0d35dfda9d3bc07aae77
anastasia <anyasirman@outlook.com>
2022-10-05 16:57:27 -0400

    removed text in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import '../styles/Footer.css'
 const Footer = () => {
     return (
         <footer id='footer'>
-        This is the footer it is going to have some social media stuff and maybe some usefull links
+
       </footer>
     )
 }
