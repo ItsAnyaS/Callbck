af63fab2f7b0a1913a3900c37fce33a1e052b3c9
anastasia <anyasirman@outlook.com>
2022-10-10 15:40:42 -0400

    added extra section in cta secton on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ const navigate = useNavigate()
     <section id='hero'>
         <div id='hero-container'>
         <h1>Quick, transparent, convientent.</h1>
-        <h3>This is a short sentence about why you should make an account</h3>
+        <h3>Callbck is a listing website that's fast, free, and transparent—all while keeping you in the loop on the application process. You can apply for jobs directly from the site, so you don't have to worry about any paper work or waiting around.</h3>
         <button onClick={()=> {navigate('/signup')}}>Get Started</button>
         </div>
         <img src='./hero-img.png' id='hero-img' alt='Dancer doing a stag leap over the home secton'/>
@@ -18,9 +18,27 @@ const navigate = useNavigate()
         <button onClick={()=> {navigate('/signup')}} >Sign Up</button>
     </section>
     <section id='company-cta-section'>
+        <section className='company-cta-illistration'>
+            <div className='company-cta-illistrations-container'>
+                <div>
+                <ion-icon name="clipboard-outline"></ion-icon>
+                <p>Sign Up.</p>
+                </div>
+                <div className='company-cta-illistrations-container-middle'>
+                <ion-icon name="create-outline"></ion-icon>
+                <p>Create a listing.</p>
+                </div>
+                <div>
+                <ion-icon name="checkbox-outline"></ion-icon>
+                <p>Done.</p>
+                </div>
+            </div>
+        </section>
+        <section className='company-cta-copy'>
         <h1>Ready.  Set.  List.</h1>
         <p>Creating a listing is as easy as 1. Sign Up 2. Fill out brief listing form, 3. done.  Our streamlined process allows you to create a listing in as little as three clicks.</p>
         <button>Join the community</button>
+        </section>
     </section>
 
 </main>
