b155016c2810bf02d420380d54d825578efed12a
anastasia <anyasirman@outlook.com>
2023-04-19 15:20:47 -0400

    removed console log from search

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -14,7 +14,6 @@ const Search = () => {
                 body: JSON.stringify(searchParams)
             })
             let res = await req.json()
-            // console.log(res)
             setListings(res)
         }
 
